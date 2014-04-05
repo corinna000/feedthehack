8658b6b22ec7999be360ed3248e1cc2443f6e7db
Andy McConnell <andrew.mcconnell@gmail.com>
2014-04-05 17:01:16 -0400

    Work, damn you

--- module/User/src/User/UserPersistenceInterface.php
+++ module/User/src/User/UserPersistenceInterface.php
@@ -3,7 +3,7 @@ namespace User;
 
 interface UserPersistenceInterface
 {
-    public function save($data);
+    public function save(\User\Entity\User $user);
     public function fetch($id);
     public function fetchAll();
 }
